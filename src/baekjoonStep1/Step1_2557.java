2df64bb51fa1ead38eefbea8cb7ccf61546afdc6
OpenBible3438 <59015538+OpenBible3438@users.noreply.github.com>
2020-02-25 22:48:57 +0900

    2단계10718번

--- src/baekjoonStep1/Step1_2557.java
+++ src/baekjoonStep1/Step1_2557.java
@@ -1,5 +1,5 @@
 package baekjoonStep1;
-public class Q2557 {
+public class Step1_2557 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ src/baekjoonStep1/Step2_10718.java
@@ -0,0 +1,10 @@
+package baekjoonStep1;
+
+public class Step2_10718 {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.print("강한친구 대한육군");
+	}
+
+}
