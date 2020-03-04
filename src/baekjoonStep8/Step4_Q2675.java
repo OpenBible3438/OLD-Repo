180f967402044ab8b4910d06308879e734d3a33c
OpenBible3438 <59015538+OpenBible3438@users.noreply.github.com>
2020-03-04 19:20:11 +0900

    200304 8단계 문자열

---
+++ src/baekjoonStep8/Step4_Q2675.java
@@ -0,0 +1,14 @@
+package baekjoonStep8;
+
+import java.util.Scanner;
+
+public class Step4_Q2675 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i=0; i<t; i++) {//테스트케이스만큼 반복
+			
+		}
+	}
+}
