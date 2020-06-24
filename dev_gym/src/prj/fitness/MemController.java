dc9750cc8dca9386684f3dd9356ffed6f95cd644
jgpark0830 <jgpark0830@gmail.com>
2020-06-24 15:38:00 +0900

    반영

--- dev_gym/src/prj/fitness/MemController.java
+++ dev_gym/src/prj/fitness/MemController.java
@@ -11,106 +11,101 @@ import org.apache.log4j.Logger;
 
 public class MemController implements Controller {
 
-	Logger logger = Logger.getLogger(MemController.class);
-	Map<String, Object> pMap = null;
-	MemLogic mLogic = null;
-	String work = null;
-	String reqName = null; 
-	int result = 0;
-	String autoSel = "false";//테이블 자동 검색 - true, false:수동
-	
-	public MemController(Map<String, Object> pMap) {
-		logger.info("MemController 생성자 호출");
-		this.pMap = pMap;
-		mLogic = new MemLogic();
-		work = pMap.get("work").toString();
-		reqName = pMap.get("reqName").toString();
-		logger.info("work : " + work + ", reqName : " + reqName);
-	}
-	
-	
-	@Override
-	public String process(String cud, HttpServletRequest req, HttpServletResponse res)
-			throws IOException, ServletException {
-		logger.info("MemController - String 타입 process 호출");
-		String path = null;
-		switch(cud) {
-			case "ins":{
-				switch(reqName) {
-					case "memInbodyIns":{
-						result = mLogic.memInbodyIns(pMap);
-					}break;
+   Logger logger = Logger.getLogger(MemController.class);
+   Map<String, Object> pMap = null;
+   MemLogic mLogic = null;
+   String work = null;
+   String reqName = null; 
+   int result = 0;
+   String autoSel = "false";
+   
+   public MemController(Map<String, Object> pMap) {
+      logger.info("MemController 생성자 호출");
+      this.pMap = pMap;
+      mLogic = new MemLogic();
+      work = pMap.get("work").toString();
+      reqName = pMap.get("reqName").toString();
+      logger.info("work : " + work + ", reqName : " + reqName);
+   }
+   
+   
+   @Override
+   public String process(String cud, HttpServletRequest req, HttpServletResponse res)
+         throws IOException, ServletException {
+      logger.info("MemController - String 타입 process 호출");
+      String path = null;
+      switch(cud) {
+         case "ins":{
+            switch(reqName) {
+               case "memInbodyIns":{
+                  result = mLogic.memInbodyIns(pMap);
+               }break;
+               case "memIns":{
+                  result = mLogic.memIns(pMap);
+               }break;
+            }
+            path = "redirect:../insertResult" + ":" + result + ":"+ autoSel;
+         }break;
+         case "upd":{
+            switch(reqName) {
+               case "memInbodyUpd":{
+                  result = mLogic.memInbodyUpd(pMap);
+               }break;
+               case "memUpd":{
+                  result = mLogic.memUpd(pMap);
+               }break;
+            }
+            path = "redirect:../updateResult:"+result + ":"+ autoSel;
+         }break;
+         case "del":{
+            switch(reqName) {
+               case "memInbodyDel":{
+                  result = mLogic.memInbodyDel(pMap);
+               }break;
+               case "memDel":{
+                  result = mLogic.memDel(pMap);
+               }break;
+            }
+            path = "redirect:../deleteResult:"+result + ":"+ autoSel;
+         }break;
+      }
+      
+      
+      
+      path = "redirect:" + work + ":" + reqName + ":" + result;
+      logger.info("path : " + path);
+      
+      return path;
+   }
 
-					case "memIns":
-
-					case "jsonMemList":{
-						logger.info("전체 조회 호출 성공:");
-						result = mLogic.memIns(pMap);
-					}break;
-				}
-				path = "redirect:../insertResult" + ":" + result + ":"+ autoSel;
-			}break;
-			case "upd":{
-				switch(reqName) {
-					case "memInbodyUpd":{
-						result = mLogic.memInbodyUpd(pMap);
-					}break;
-					case "memUpd":{
-						result = mLogic.memUpd(pMap);
-					}break;
-				}
-				path = "redirect:../updateResult:"+result + ":"+ autoSel;
-			}break;
-			case "del":{
-				switch(reqName) {
-					case "memInbodyDel":{
-						result = mLogic.memInbodyDel(pMap);
-					}break;
-					case "memDel":{
-						result = mLogic.memDel(pMap);
-					}break;
-				}
-				path = "redirect:../deleteResult:"+result + ":"+ autoSel;
-			}break;
-		}
-		
-		
-		
-		path = "redirect:" + work + ":" + reqName + ":" + result;
-		logger.info("path : " + path);
-		
-		return path;
-	}
-
-	@Override
-	public ModelAndView process(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-		logger.info("MemController - mav 타입 process 호출");
-		ModelAndView mav = new ModelAndView(req, res);
-		Object selResult = null;
-		switch(reqName){
-			case "jsonMemDetail":{
-				selResult = mLogic.getMemDetail(pMap);
-			}break;
-			case "jsonMemInbody":{
-				selResult = mLogic.getMemInbody(pMap);
-			}break;
-			case "jsonMemList":{
-				selResult = mLogic.getMemList(pMap);
-			}break;
-		}
-		if(selResult != null) {
-			logger.info("selResult != null");
-			mLogic.getMemList(pMap);
-			mav.addObject("selResult", selResult);
-			mav.setViewName(work+"/"+reqName);
-		}
-		else {
-			logger.info("selResult == null");
-			logger.info("MemController - selResult가 Null입니다.");
-		}
-		
-		
-		return mav;
-	}
+   @Override
+   public ModelAndView process(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+      logger.info("MemController - mav 타입 process 호출");
+      ModelAndView mav = new ModelAndView(req, res);
+      Object selResult = null;
+      switch(reqName){
+         case "jsonMemDetail":{
+            selResult = mLogic.getMemDetail(pMap);
+         }break;
+         case "jsonMemInbody":{
+            selResult = mLogic.getMemInbody(pMap);
+         }break;
+         case "jsonMemList":{
+            selResult = mLogic.getMemList(pMap);
+         }break;
+      }
+      if(selResult != null) {
+         logger.info("selResult != null");
+         mav.addObject("selResult", selResult);
+         mav.setViewName(work+"/"+reqName);
+      }
+      else {
+         logger.info("selResult == null");
+         logger.info("MemController - selResult가 Null입니다.");
+      }
+      
+      
+      return mav;
+   }
 
 }
