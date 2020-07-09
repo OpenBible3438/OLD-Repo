80a3fd114a3e7476db16dde25a96ad67ccb4e9f8
jgpark0830 <jgpark0830@gmail.com>
2020-07-09 17:10:40 +0900

    0709 준규

--- app/src/main/java/com/kosmo59/yoginaegym/member/MyreviewRegDialog.java
+++ app/src/main/java/com/kosmo59/yoginaegym/member/MyreviewRegDialog.java
@@ -1,24 +1,65 @@
 package com.kosmo59.yoginaegym.member;
 
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
 import com.kosmo59.yoginaegym.R;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.util.zip.Inflater;
 
 
 public class MyreviewRegDialog {
     private Context context;
     private Spinner reviewInsSpinner;
     private String className = "";
+    private Button btn_revIns1;
+
+
 
     public MyreviewRegDialog(Context context) {
         this.context = context;
@@ -54,12 +95,37 @@ public class MyreviewRegDialog {
                 className = reviewInsSpinner.getItemAtPosition(position).toString();
                 /*Toast.makeText(context, className+"선택", Toast.LENGTH_SHORT).show();*/
             }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {  }
+        });
+        /* 등록 버튼 시작 */
+        Button button = dlg.findViewById(R.id.btn_revIns1);
+        button.setOnClickListener(new View.OnClickListener(){
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onClick(View v) {
+                show();
             }
         });
     }
-
+    void show(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("등록하시겠습니까?");
+        builder.setPositiveButton("예",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context,"등록되었습니다.",Toast.LENGTH_LONG ).show();
+                    }
+                });
+        builder.setNegativeButton("아니오",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context,"취소되었습니다.",Toast.LENGTH_LONG).show();
+                    }
+                });
+        builder.show();
+    }
 }
+
