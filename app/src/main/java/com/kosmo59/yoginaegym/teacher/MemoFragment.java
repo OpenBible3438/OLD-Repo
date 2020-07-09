992e70758ebb96ee7ce4e7354173ce318c0c4eb3
RavenBin <ravenbin5829@gmail.com>
2020-07-09 20:12:02 +0900

    0709 현빈 강사 끝

--- app/src/main/java/com/kosmo59/yoginaegym/teacher/MemoFragment.java
+++ app/src/main/java/com/kosmo59/yoginaegym/teacher/MemoFragment.java
@@ -1,17 +1,23 @@
 package com.kosmo59.yoginaegym.teacher;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.kosmo59.yoginaegym.R;
 
 public class MemoFragment extends Fragment {
-
+    private Context context;
+    private Button tch_memo;
     public MemoFragment() {
         // Required empty public constructor
     }
@@ -20,7 +26,34 @@ public class MemoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memo, container, false);
+        View view = inflater.inflate(R.layout.fragment_memo, container, false);
+        context = container.getContext();
+        tch_memo =view.findViewById(R.id.tch_memo);
+        tch_memo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(context,"등록 클릭.",Toast.LENGTH_LONG).show();
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setMessage("메모를 하시겠습니까?");
+                alertDialogBuilder.setPositiveButton("등록", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(context,"등록되었습니다.",Toast.LENGTH_LONG).show();
+                    }
+                });
+                alertDialogBuilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context,"취소되었습니다.",Toast.LENGTH_LONG).show();
+                        //finish();
+                    }
+                });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+        });
+        //return inflater.inflate(R.layout.fragment_my_review, container, false);
+        return view;
     }
+
 }
