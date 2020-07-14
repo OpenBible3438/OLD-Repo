18a980c702e8bbe488fa4fdc74bc8c3003589708
kimsh0306 <b666790@gmail.com>
2020-07-14 20:47:37 +0900

    커밋

--- app/src/main/java/com/kosmo59/yoginaegym/teacher/TchMemListAdaper.java
+++ app/src/main/java/com/kosmo59/yoginaegym/teacher/TchMemListAdaper.java
@@ -48,45 +48,6 @@ public class TchMemListAdaper extends ArrayAdapter {
         return mList.get(position);
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(this.resourceId, parent, false);
-        /*final View view = mLayoutInflater.inflate(R.layout.tchclasslistview_item, null);*/
-        Log.i(TAG, "■■■■■■■■ position : " + position);
 
-        CardView tch_class = (CardView) convertView.findViewById(R.id.tch_class);
-        TextView cls_name = (TextView) convertView.findViewById(R.id.cls_name);
-        TextView cls_stime = (TextView) convertView.findViewById(R.id.cls_stime);
-        TextView cls_etime = (TextView) convertView.findViewById(R.id.cls_etime);
-        TextView cls_day = (TextView) convertView.findViewById(R.id.cls_day);
-        TextView cls_cnt = (TextView) convertView.findViewById(R.id.cls_cnt);
-
-
-        cls_name.setText(mList.get(position).get("CLS_NAME").toString());
-        cls_stime.setText(mList.get(position).get("CLS_STIME").toString());
-        cls_etime.setText(mList.get(position).get("CLS_ETIME").toString());
-        cls_day.setText(mList.get(position).get("CLS_DAY").toString());
-        cls_cnt.setText(""+Integer.valueOf((int)Math.round((double)mList.get(position).get("MEM_NUM"))));
-        mList.get(position).get("MEM_NUM");
-        final View finalConvertView = convertView;
-        tch_class.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
-                final Dialog dlg = new Dialog(finalConvertView.getContext());
-
-                // 액티비티의 타이틀바를 숨긴다.
-                dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-                // 커스텀 다이얼로그의 레이아웃을 설정한다.
-                dlg.setContentView(R.layout.dialog_tch_mem_list);
-
-                // 커스텀 다이얼로그를 노출한다.
-                dlg.show();
-            }
-        });
-        return convertView;
-    }
 
 }
