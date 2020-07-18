791d986ec100f7b6f340885b78b2f93b157aad92
hyelim9210 <gppfla210@naver.com>
2020-07-18 18:25:48 +0900

    컨텐츠 완성

--- app/src/main/java/com/kosmo59/yoginaegym/member/MemContentActivity.java
+++ app/src/main/java/com/kosmo59/yoginaegym/member/MemContentActivity.java
@@ -47,13 +47,10 @@ public class MemContentActivity extends AppCompatActivity {
 
     public final String MEM_CONT_LOG = "MemContentActivity";
 
-    //1이면 눌려있을 때, 0이면 안 눌려있을 때
-    private int check_num = 0;
     //이미지 버튼
     private ImageButton ib_emptyHeart;
     private ListView lv_mem_cont;
-    //좋아요 수
-    private int like=0;
+
     private TextView tv_contLike;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +81,7 @@ public class MemContentActivity extends AppCompatActivity {
                 for (int i = 0; i < jsonArray.length(); i++) {
                     contMap = new HashMap<>();
                     jsonObject = jsonArray.getJSONObject(i);
-                    contMap.put("cont_cont", jsonObject.getString("CONT_CONT"));
+                    contMap.put("CONT_CONT", jsonObject.getString("CONT_CONT"));
                     contMap.put("CONT_SEQ", jsonObject.getString("CONT_SEQ"));
                     contMap.put("CONT_LIKE", jsonObject.getString("CONT_LIKE"));
                     contMap.put("CONT_DATE", jsonObject.getString("CONT_DATE"));
@@ -101,7 +98,7 @@ public class MemContentActivity extends AppCompatActivity {
             lv_mem_cont = findViewById(R.id.lv_mem_cont);
             lv_mem_cont.setAdapter(memContentAdapter);
 
-
+            ib_emptyHeart = findViewById(R.id.ib_emptyHeart1);
 
             //////////////////////////////////////////////////////////////////////////////////////
             //////////////////////////////////////////////////////////////////////////////////////
@@ -162,23 +159,4 @@ public class MemContentActivity extends AppCompatActivity {
         }
     }
 
-    public void cont_clickHeart(View view) {
-        ib_emptyHeart = findViewById(view.getId());
-        //0이면 눌려있지 않을 때 => 빈 하트
-        if(check_num == 0){
-            //눌려있지 않은 상태에서 눌렸으면 1로 바꾸어주기 => 눌린상태
-            check_num = 1;
-            ib_emptyHeart.setSelected(true); //이미지버튼이 선택된 상태니까 true
-            ib_emptyHeart.setImageResource(R.drawable.cont_heart); //눌렸으니까 색칠되어있는 하트 이미지로 변경
-            like+=1;
-            //tv_contLike.setText(like+"명이 좋아합니다");
-        }
-        else{  //이미지 버튼이 눌려있을 때 (check_num == 1)인 상태
-            check_num = 0; //눌려있을 때 다시 누르면 안 눌린 상태로 돌려야되니까 check_num=0으로
-            ib_emptyHeart.setSelected(false); //이미지 버튼이 선택 안 되어있으니까 상태 false
-            ib_emptyHeart.setImageResource(R.drawable.cont_heart_empty); //다시 눌리지 않은 상태니까 이미지를 빈 하트 이미지로 변경
-            like-=1;
-            //tv_contLike.setText(like+"명이 좋아합니다");
-        }
-    }
 }
