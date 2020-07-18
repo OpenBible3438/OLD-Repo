6eccfcbbdda2588c06af89cb071966d037a0b027
OpenBible3438 <zzagsk@gmail.com>
2020-07-18 18:08:37 +0900

    성경 강사 채팅 회원 리스트

--- app/src/main/java/com/kosmo59/yoginaegym/teacher/TchChatAdapter.java
+++ app/src/main/java/com/kosmo59/yoginaegym/teacher/TchChatAdapter.java
@@ -2,6 +2,7 @@ package com.kosmo59.yoginaegym.teacher;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +16,12 @@ import androidx.cardview.widget.CardView;
 import com.kosmo59.yoginaegym.R;
 import com.kosmo59.yoginaegym.common.AppVO;
 import com.kosmo59.yoginaegym.common.Chat;
+import com.kosmo59.yoginaegym.common.TomcatImg;
 
 import java.util.List;
 import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class TchChatAdapter extends ArrayAdapter {
     Context context;
@@ -61,10 +65,23 @@ public class TchChatAdapter extends ArrayAdapter {
         final TextView tv_tch_chat_gymName = convertView.findViewById(R.id.tv_tch_chat_gymName);
         final TextView tv_tch_chat_clsName = convertView.findViewById(R.id.tv_tch_chat_clsName);
         final CardView cv_chat_list = convertView.findViewById(R.id.cv_chat_list);
+        //사진 추가 - 성경
+        final CircleImageView iv_tch_chat_mem_img = convertView.findViewById(R.id.iv_tch_chat_mem_img);
 
         tv_tch_chat_memName.setText(chatMemList.get(position).get("MEM_NAME").toString());
         tv_tch_chat_gymName.setText(chatMemList.get(position).get("MEM_GENDER").toString());
         tv_tch_chat_clsName.setText(chatMemList.get(position).get("MEM_ADDR").toString());
+
+        try{
+            TomcatImg tomcatImg = new TomcatImg();
+            String imsi = chatMemList.get(position).get("FILE_SEQ").toString().substring(0, chatMemList.get(position).get("FILE_SEQ").toString().length()-2);
+            Log.i("TchChatAdapter:", "file_seq : "+imsi);
+            String bitImg = tomcatImg.execute(imsi).get();
+            Bitmap bitmap = tomcatImg.getBitMap(bitImg);
+            iv_tch_chat_mem_img.setImageBitmap(bitmap);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
         vo = (AppVO)context.getApplicationContext();
         cv_chat_list.setOnClickListener(new View.OnClickListener() {
