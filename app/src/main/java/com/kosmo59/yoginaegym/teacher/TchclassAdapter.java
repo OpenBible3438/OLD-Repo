bc9aa61d5790f76a812011349a64f8bec2da897c
hjho <gjwnsgh131@naver.com>
2020-07-19 18:25:49 +0900

    0719 준호

--- app/src/main/java/com/kosmo59/yoginaegym/teacher/TchclassAdapter.java
+++ app/src/main/java/com/kosmo59/yoginaegym/teacher/TchclassAdapter.java
@@ -108,10 +108,8 @@ public class TchclassAdapter extends ArrayAdapter {
             // 커스텀 다이얼로그를 정의하기위해 Dialog클래스를 생성한다.
 
             final Dialog dlg = new Dialog(finalConvertView.getContext());
-
             // 액티비티의 타이틀바를 숨긴다.
             dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
             // 커스텀 다이얼로그의 레이아웃을 설정한다.
             dlg.setContentView(R.layout.dialog_tch_mem_list);
             // 커스텀 다이얼로그를 노출한다.
